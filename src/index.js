f2ff8cafb08be4b44dca9b6c9d44d91da2985be3
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2023-11-01 23:55:03 +0530

    firebase configuration

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { router as blogsRouter} from './routes/blogs.js';
 import { createConenction } from './dbconfig/mongoConnection.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import helmet from 'helmet';
+import cors from 'cors';
+import * as functions from 'firebase-functions';
 
 const app = express();
 
 // middlewares
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
 
 createConenction(); // db connection
 
@@ -19,3 +22,5 @@ app.use('/blogs', blogsRouter);
 app.listen(3000, ()=>{
 	console.log('listening on port 3000');
 });
+
+exports.api = functions.https.onRequest(app);
