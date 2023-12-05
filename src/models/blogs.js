f165571ec59b5d0e545d8e1d3ce8b0825a2c4177
Ajinkya Prakash Dhomne <ajinkya.10730701@ltimindtree.com>
2023-12-05 12:08:00 +0530

    issue: fixed key mismatch

--- src/models/blogs.js
+++ src/models/blogs.js
@@ -1,18 +1,18 @@
 import { mongoose } from "mongoose";
 const Schema = mongoose.Schema;
 const blogsSchema = new Schema(
-	{
-		userId: String,
-		title: String,
-		body: String,
-		comments: [
-			{
-				userName: String,
-				comments: String,
-			},
-		],
-	},
-	{ versionKey: false }
+  {
+    userId: String,
+    title: String,
+    body: String,
+    comments: [
+      {
+        userName: String,
+        comment: String,
+      },
+    ],
+  },
+  { versionKey: false }
 );
 
 const blogs = mongoose.model("blogs", blogsSchema, "blogs");
